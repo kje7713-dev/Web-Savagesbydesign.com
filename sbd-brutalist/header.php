f1c9c6549b903c0f6a6b3451ec62674a7723019c
kje7713-dev <kje7713@gmail.com>
2025-12-23 18:05:03 -0500

    Create header.php

---
+++ sbd-brutalist/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header class="site-header">
+  <h1><a href="<?php echo home_url(); ?>">Savage by Design</a></h1>
+</header>
